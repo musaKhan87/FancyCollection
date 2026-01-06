711f1154bc41add1e9bf1c37c3cd698b8a5da3d2
Musa Khan <16khanmusa@gmail.com>
2026-01-06 17:55:26 +0530

    adding app settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { VitePWA } from "vite-plugin-pwa";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["logo.jpeg", "logo.jpeg"],
+      manifest: {
+        name: "Fancy Collection",
+        short_name: "FC",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ff6a00",
+      },
+    }),
+  ],
+});
